d071e058616b3d1184298c7c749417414dc3e739
starboyhassan <starboyhassan11@gmail.com>
2023-08-09 18:57:47 +0300

    Second commit

--- filemanager/admin.py
+++ filemanager/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Directory, File
+
 # Register your models here.
+
+admin.site.register(Directory)
+
+admin.site.register(File)
--- filemanager/models.py
+++ filemanager/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+#################################
+
+
+class Directory(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to="files/")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
