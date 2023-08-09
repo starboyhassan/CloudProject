2605f255fea8f0aa3246052d643b026ad1996326
starboyhassan <starboyhassan11@gmail.com>
2023-08-09 19:12:16 +0300

    Third commit

--- account/models.py
+++ account/models.py
@@ -1,8 +1,3 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-
-
-class CustomUser(AbstractUser):
-    custom_field = models.CharField(max_length=50)
