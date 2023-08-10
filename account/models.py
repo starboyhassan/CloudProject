feb2f5638250888e71797af350d127addacec083
starboyhassan <starboyhassan11@gmail.com>
2023-08-10 18:19:13 +0300

    fourth commit

--- account/models.py
+++ account/models.py
@@ -1,3 +1,58 @@
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
+from django.forms import ValidationError
 
 # Create your models here.
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        if not password:
+            raise ValidationError("password required")
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        user = self.create_user(
+            email=email, password=password, is_staff=True, is_superuser=True
+        )
+        return user
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    first_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=11, unique=True)
+    city = models.ForeignKey(
+        "City",
+        related_name="user_cities",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+
+class City(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return str(self.name)
--- filemanager/models.py
+++ filemanager/models.py
@@ -1,5 +1,6 @@
-from django.contrib.auth.models import User
 from django.db import models
+
+from account.models import CustomUser
 
 #################################
 
@@ -8,7 +9,7 @@ class Directory(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -18,7 +19,7 @@ class File(models.Model):
     name = models.CharField(max_length=255)
     file = models.FileField(upload_to="files/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
 
     def __str__(self):
